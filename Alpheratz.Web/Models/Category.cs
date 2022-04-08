985cebd405ad30231143f1d0ebb954b5a52a995d
iamstan13y <iamstan13y@gmail.com>
2022-04-08 08:50:29 +0200

    Category Model & Data Annotations.

---
+++ Alpheratz.Web/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Alpheratz.Web.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+    }
+}
