5c6847e1f3023616f1f0d36de8a917c69a60286c
iamstan13y <iamstan13y@gmail.com>
2022-04-08 12:42:21 +0200

    Retrive all Categories.

--- source/Alpheratz.Web/Controllers/CategoryController.cs
+++ source/Alpheratz.Web/Controllers/CategoryController.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Alpheratz.Web.Models.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Alpheratz.Web.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            var categories = _context.Categories!.ToList();
             return View();
         }
     }
