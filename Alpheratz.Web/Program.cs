93502c987abbf61888b918df4234543c64d1c0c4
iamstan13y <iamstan13y@gmail.com>
2022-04-08 09:17:58 +0200

    Configure Program.cs to use DbContext.

--- Alpheratz.Web/Program.cs
+++ Alpheratz.Web/Program.cs
@@ -1,7 +1,14 @@
+using Alpheratz.Web.Models.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
 var app = builder.Build();
 
