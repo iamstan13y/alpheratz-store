3c264ba4fcb988442dd2c4b570f942ff7652c0ff
iamstan13y <iamstan13y@gmail.com>
2022-04-08 09:12:36 +0200

    NuGet Packages & AppDbContext.

---
+++ Alpheratz.Web/Models/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Alpheratz.Web.Models.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category>? Categories { get; set; }
+    }
+}
