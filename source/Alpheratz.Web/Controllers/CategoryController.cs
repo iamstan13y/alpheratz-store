623e98f9c0310f7f8b9e29145560e50e5abb6d7c
iamstan13y <iamstan13y@gmail.com>
2022-04-12 16:44:37 +0200

    Edit Category - GET

--- source/Alpheratz.Web/Controllers/CategoryController.cs
+++ source/Alpheratz.Web/Controllers/CategoryController.cs
@@ -45,5 +45,40 @@ namespace Alpheratz.Web.Controllers
 
             return View(obj);
         }
+        
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            var category = _context.Categories!.Find(id);
+
+            if (category == null)
+                return NotFound();
+
+            return View(category);
+        }
+        
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot exactly match the name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Categories!.Add(obj);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
     }
 }
