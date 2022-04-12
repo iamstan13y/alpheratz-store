01c4f55cd114f04197bc990bbfd05124d9a5ee06
iamstan13y <iamstan13y@gmail.com>
2022-04-12 14:59:54 +0200

    Display Name & Range Validation.

--- source/Alpheratz.Web/Models/Category.cs
+++ source/Alpheratz.Web/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Alpheratz.Web.Models
 {
@@ -8,6 +9,8 @@ namespace Alpheratz.Web.Models
         public int Id { get; set; }
         [Required]
         public string? Name { get; set; }
+        [DisplayName("Display Name")]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1 and 100.")]
         public int DisplayOrder { get; set; }
         public DateTime DateCreated { get; set; } = DateTime.Now;
     }
