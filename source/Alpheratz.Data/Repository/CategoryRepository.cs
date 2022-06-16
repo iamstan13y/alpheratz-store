915cb75418efbd2cc6c9294896e13ada09fb5562
iamstan13y <iamstan13y@gmail.com>
2022-06-16 14:53:43 +0200

    Category Repository implementation.

---
+++ source/Alpheratz.Data/Repository/CategoryRepository.cs
@@ -0,0 +1,52 @@
+﻿using Alpheratz.DataAccess.Data;
+using Alpheratz.DataAccess.Repository.IRepository;
+using Alpheratz.ModelLibrary.Models;
+using System.Linq.Expressions;
+
+namespace Alpheratz.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private AppDbContext _context;
+
+        public CategoryRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public void Add(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Category> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetFirstOrDefault(Expression<Func<Category, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<Category> entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
