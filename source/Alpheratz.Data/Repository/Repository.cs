50b564f7e3cdd5991578a354c8ded48fe032d826
iamstan13y <iamstan13y@gmail.com>
2022-06-16 14:33:40 +0200

    IRepository implementation.

---
+++ source/Alpheratz.Data/Repository/Repository.cs
@@ -0,0 +1,43 @@
+﻿using Alpheratz.DataAccess.Data;
+using Alpheratz.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alpheratz.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly AppDbContext _context;
+        internal DbSet<T> _dbSet;
+
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetFirstOrDefault(System.Linq.Expressions.Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
