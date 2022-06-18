9b732064e0621b45e930bbf8585b03b97b73af25
iamstan13y <iamstan13y@gmail.com>
2022-06-18 21:45:53 +0200

    Cover Type Repository.

---
+++ source/Alpheratz.Data/Repository/CoverTypeRepository.cs
@@ -0,0 +1,18 @@
+﻿using Alpheratz.DataAccess.Data;
+using Alpheratz.DataAccess.Repository.IRepository;
+using Alpheratz.ModelLibrary.Models;
+
+namespace Alpheratz.DataAccess.Repository
+{
+    public class CoverTypeRepository : Repository<CoverType>, ICoverTypeRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CoverTypeRepository(AppDbContext context) : base(context) => _context = context;
+
+        public void Update(CoverType coverType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
