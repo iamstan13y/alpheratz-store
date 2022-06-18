095a4755dd859caf929c7eb40e77f8b4deeb86c8
iamstan13y <iamstan13y@gmail.com>
2022-06-18 22:05:03 +0200

    Cover Type model enhancements.

--- source/Alpheratz.ModelLibrary/Models/CoverType.cs
+++ source/Alpheratz.ModelLibrary/Models/CoverType.cs
@@ -1,8 +1,13 @@
-﻿namespace Alpheratz.ModelLibrary.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Alpheratz.ModelLibrary.Models
 {
     public class CoverType
     {
         public int Id { get; set; }
+        [Display(Name = "Cover Type")]
+        [Required]
+        [MaxLength(50)]
         public string? Name { get; set; }
     }
 }
