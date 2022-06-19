8e8c172bc3b04fa5cf09730f9df8292e9d8d5392
iamstan13y <iamstan13y@gmail.com>
2022-06-19 19:55:36 +0200

    Cover Type Controller.

---
+++ source/Alpheratz.Web/Areas/Admin/Controllers/CoverTypeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Alpheratz.Web.Areas.Admin.Controllers
+{
+    public class CoverTypeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
