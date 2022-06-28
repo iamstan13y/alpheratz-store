754aae965eadbbce389b4111571e973cc9d1d5f3
iamstan13y <iamstan13y@gmail.com>
2022-06-28 23:24:32 +0200

    Product Controller - III.

--- source/Alpheratz.Web/Areas/Admin/Controllers/ProductController.cs
+++ source/Alpheratz.Web/Areas/Admin/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using Alpheratz.DataAccess.Repository.IRepository;
+using Alpheratz.ModelLibrary.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Alpheratz.Web.Areas.Admin.Controllers
@@ -11,7 +12,7 @@ namespace Alpheratz.Web.Areas.Admin.Controllers
 
         public IActionResult Index()
         {
-            IEnumerable<Product> categories = _unitOfWork..GetAll();
+            IEnumerable<CoverType> categories = _unitOfWork.CoverType.GetAll();
             return View(categories);
         }
 
